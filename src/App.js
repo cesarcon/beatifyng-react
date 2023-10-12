0a7ba427fcc69cfefba61df3a5e752ac82c88842
Cesar Contreras <contreras.cesar53@gmail.com>
2023-10-11 21:01:33 -0500

    Se crean los componentes de NavBar, Intro, y productList

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { NavBar } from "./NavBar/NavBar";
+import { Intro } from "./Intro/Intro";
+import { ProductsList } from "./ProductList/ProductList";
+import { Product } from "./Product/Product";
 
 function App() {
+  const listaProductos = [
+    {urlImg: 'https://images.pexels.com/photos/3997391/pexels-photo-3997391.jpeg?auto=compress&cs=tinysrgb&w=1600', 
+    precio: 12000, titulo: 'Manicure'},
+    {urlImg: 'https://images.pexels.com/photos/2040189/pexels-photo-2040189.jpeg?auto=compress&cs=tinysrgb&w=1600', 
+    precio: 18000, titulo: 'Corte basico hombre'},
+    {urlImg: 'https://images.pexels.com/photos/3993312/pexels-photo-3993312.jpeg?auto=compress&cs=tinysrgb&w=1600', 
+    precio: 80000, titulo: 'Tintura'},
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Intro />
+      <ProductsList>
+        {listaProductos.map(producto => (
+          <Product urlImg={producto.urlImg} precio={producto.precio} titulo={producto.titulo}/>
+        )
+        )}
+      </ProductsList>
+    </>
   );
 }
 
